99f6b04e0c8ac9392cc61908e8a0c163e4087956
Vivek <vkkr04@gmail.com>
2024-02-27 14:07:53 +0530

    remove duplicate files and deploy again

--- server/database/dbConnect.js
+++ server/database/dbConnect.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const mongoose = require("mongoose");
 
 mongoose
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 // CORS middleware
 router.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
